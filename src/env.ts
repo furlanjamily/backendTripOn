9464626c53f3e809e05a418e0f14073c2dbeab6a
furlanjamily <102761258+furlanjamily@users.noreply.github.com>
2024-09-21 20:30:12 -0300

    CommitAjustDeploy

--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,9 @@
-import { z } from 'zod'
+import { z } from 'zod';
 
 const envSchema = z.object({
-  DATABASE_URL: z.string().url(),
-  WEB_BASE_URL: z.string().url(),
-  PORT: z.coerce.number().default(3333),
-})
+  DATABASE_URL: z.string().url('postgresql://nodedeploy_pg_1708_user:B6C69UavWUA9Lk6PYtMmhYyNMVESLagf@dpg-crn3m2g8fa8c73892mr0-a/nodedeploy_pg_1708'),
+  WEB_BASE_URL: z.string().url('https://journey-chi-ten.vercel.app'),
+  PORT: z.number().int('PORT must be an integer').default(3333), // Primeiro verifica se é um inteiro, depois define o valor padrão
+});
 
-export const env = envSchema.parse(process.env)
+export const env = envSchema.parse(process.env);
