5af74108928019a0bb69c248bcc0856effef3dc5
furlanjamily <102761258+furlanjamily@users.noreply.github.com>
2024-09-21 21:37:03 -0300

    commitEnv

--- src/env.ts
+++ src/env.ts
@@ -3,7 +3,15 @@ import { z } from 'zod';
 const envSchema = z.object({
   DATABASE_URL: z.string().url('postgresql://nodedeploy_pg_1708_user:B6C69UavWUA9Lk6PYtMmhYyNMVESLagf@dpg-crn3m2g8fa8c73892mr0-a/nodedeploy_pg_1708'),
   WEB_BASE_URL: z.string().url('https://journey-chi-ten.vercel.app'),
-  PORT: z.number().int('PORT must be an integer').default(3333), // Primeiro verifica se é um inteiro, depois define o valor padrão
+  PORT: z.string()
+    .transform((val) => {
+      const port = parseInt(val, 10);
+      if (isNaN(port)) {
+        throw new Error('PORT must be a valid number');
+      }
+      return port;
+    })
+    .default('3333'), // Define o valor padrão como string
 });
 
 export const env = envSchema.parse(process.env);
