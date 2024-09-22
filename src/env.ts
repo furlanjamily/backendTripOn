e4ffe70ff744c7715247fbdc5167fe5e90e1bc6e
furlanjamily <102761258+furlanjamily@users.noreply.github.com>
2024-09-21 21:54:27 -0300

    commitApiBase

--- src/env.ts
+++ src/env.ts
@@ -1,8 +1,9 @@
 import { z } from 'zod';
 
 const envSchema = z.object({
-  DATABASE_URL: z.string().url('postgresql://nodedeploy_pg_1708_user:B6C69UavWUA9Lk6PYtMmhYyNMVESLagf@dpg-crn3m2g8fa8c73892mr0-a/nodedeploy_pg_1708'),
-  WEB_BASE_URL: z.string().url('https://journey-chi-ten.vercel.app'),
+  DATABASE_URL: z.string().url(),
+  WEB_BASE_URL: z.string().url(),
+  API_BASE_URL: z.string().url().default('http://localhost:3333'), // Adicione esta linha
   PORT: z.string()
     .transform((val) => {
       const port = parseInt(val, 10);
@@ -13,5 +14,11 @@ const envSchema = z.object({
     })
     .default('3333'), // Define o valor padrão como string
 });
+
+// Defina suas variáveis de ambiente corretamente
+process.env.DATABASE_URL = "postgresql://nodedeploy_pg_1708_user:B6C69UavWUA9Lk6PYtMmhYyNMVESLagf@dpg-crn3m2g8fa8c73892mr0-a/nodedeploy_pg_1708";
+process.env.WEB_BASE_URL = "https://journey-chi-ten.vercel.app";
+process.env.API_BASE_URL = "http://localhost:3333";
+process.env.PORT = "3333";
 
 export const env = envSchema.parse(process.env);
